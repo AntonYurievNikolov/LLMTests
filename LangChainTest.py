6d3b6173a768f6caeb13de3e6b71e4eb0a489ac9
AN <antonyurievnikolov@gmail.com>
2023-04-12 15:15:53 +0300

    SPlit part 2

--- LangChainTest.py
+++ LangChainTest.py
@@ -1,33 +1,7 @@
-from langchain.document_loaders import UnstructuredMarkdownLoader, UnstructuredEPubLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-pdfs = [
-
-]
-annual_reports = []
-for pdf in pdfs:
-    #loader = UnstructuredMarkdownLoader(pdf) MarkDownFromChatGpt
-    loader = UnstructuredEPubLoader(pdf)
-    # Load the PDF document
-    document = loader.load()        
-    # Add the loaded document to our list
-    annual_reports.append(document)
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-
-chunked_annual_reports = []
-for annual_report in annual_reports:
-    # Chunk the annual_report
-    texts = text_splitter.split_documents(annual_report)
-    # Add the chunks to chunked_annual_reports, which is a list of lists
-    chunked_annual_reports.append(texts)
-    print(f"chunked_annual_report length: {len(texts)}")
-#Everything above should be split into ingesting phase
 from langchain.vectorstores import Chroma, Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from tqdm.autonotebook import tqdm
 from langchain.chains import ConversationalRetrievalChain
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.chat_models import ChatOpenAI
 import gradio as gr
 import pinecone
@@ -47,10 +21,7 @@ pinecone.init(
 )
 index_name = "esoteric"
 
-# Upsert annual reports to Pinecone via LangChain.
-# There's likely a better way to do this instead of Pinecone.from_texts()
-for chunks in chunked_annual_reports:
-    Pinecone.from_texts([chunk.page_content for chunk in chunks], embeddings, index_name=index_name) 
+
 
 vectorstore = Pinecone.from_existing_index(index_name=index_name, embedding=embeddings)
 
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # LLMTests
-Various LLM tests. Initialize with LangChain and OpenAI
+Various LLM tests. Initialize with LangChain and OpenAI. The texts are deliberately chosen to be non existant from the Original GPT 4 Dataset.
